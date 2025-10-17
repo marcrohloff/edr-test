54ad639940e35a54b512ae48fad59c3014dd6df1
Marc Rohloff <marc@marcrohloff.com>
2025-10-17 01:56:03 -0600

    Cleanup file loading

--- environment.rb
+++ environment.rb
@@ -1,9 +1,25 @@
-$LOAD_PATH << '.'
+LIB_ROOT = './lib'
+$LOAD_PATH << LIB_ROOT
 
 require 'bundler/setup'
 Bundler.require
 
+# GEM requires
 require 'active_model'
 require 'active_support'
 
-require 'lib/commands'
+module Kernel
+  # A poor man's Zeitwerk
+  def require_all(path)
+    files = Dir[File.join(LIB_ROOT, path, '**', '*.rb')]
+
+    files.each do |filename|
+      require filename
+    end
+  end
+end
+
+require     'command/base' # This need to be loaded first
+require_all 'command'
+require_all 'activity_log'
+require_all 'runner'
